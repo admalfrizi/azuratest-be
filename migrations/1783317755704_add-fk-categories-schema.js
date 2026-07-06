c8ffaa97f6c5ae6bc5a04103bcc07bfb3bf1cf35
adamalfarizi <adam.alfarizi.2002@gmail.com>
2026-07-06 14:27:50 +0700

    feat: adding for update and delete book function, fixing for entrypoint.sh due to error when running

---
+++ migrations/1783317755704_add-fk-categories-schema.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.addConstraint('books', 'fk_books_category', {
+        foreignKeys: {
+            columns: 'category_id',
+            references: 'categories(id)',
+            onDelete: 'RESTRICT',
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropConstraint('books', 'fk_books_category');
+
+    pgm.addConstraint('books', 'fk_books_category', {
+        foreignKeys: {
+        columns: 'category_id',
+        references: 'categories(id)',
+        },
+    });
+};
